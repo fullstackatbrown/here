ed3e4dfc435e850ff3daa36589b649a93451818e
Jenny Yu <jennyyu212@outlook.com>
2022-10-09 14:10:39 -0400

    feat: init package

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+
+      <h1 className={styles.title}>
+        Here!
+      </h1>
+
+    </div>
+  )
+}
+
+export default Home
