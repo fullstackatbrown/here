f018758a0db13ac3c482894461c7417f738e2278
Jenny <jennyyu212@gmail.com>
2023-03-17 13:53:04 -0400

    style: sections view

--- frontend/src/components/course/CourseAdminView/SectionsView/SectionsView.tsx
+++ frontend/src/components/course/CourseAdminView/SectionsView/SectionsView.tsx
@@ -1,19 +1,51 @@
-import { Stack } from "@mui/material";
+import { Box, Button, Stack, Typography } from "@mui/material";
+import { useSections } from "@util/section/hooks";
 import { Course } from "model/course";
+import { FC, useState } from "react";
 import Assignments from "../AssignmentsView/Assignments/Assignments";
 import AvailabilitySurvey from "./AvailabilitySurvey/AvailabilitySurvey";
 import GradeOptions from "./GradeOptions/GradeOptions";
-import Sections from "./Sections/Sections";
+import CreateEditSectionDialog from "./Sections/CreateEditSectionDialog";
+import SectionCard from "./Sections/SectionCard";
 
 export interface SectionsViewProps {
   course: Course;
 }
 
-export default function SectionsView(props: SectionsViewProps) {
+const SectionsView: FC<SectionsViewProps> = ({ course }) => {
+  const [createSectionDialog, setcreateSectionDialog] = useState(false);
+  const [sections, loading] = useSections(course.ID);
+
+  const getEnrollment = (sectionId: string) => {
+    // loop through courses.students and count the number of students whose value is section id
+    if (!course.students) return 0;
+    let count = 0;
+    Object.keys(course.students).forEach((studentId) => {
+      if (course.students[studentId] === sectionId) {
+        count++;
+      }
+    });
+    return count;
+  }
+
   return (
-    <Stack>
-      <Sections course={props.course} />
-      <AvailabilitySurvey course={props.course} />
-    </Stack>
+    <>
+      <CreateEditSectionDialog open={createSectionDialog} onClose={() => setcreateSectionDialog(false)} courseID={course.ID}
+      />
+      <Stack direction="row" justifyContent="space-between" mb={1}>
+        <Typography variant="h6" fontWeight={600}>
+          Sections
+        </Typography>
+        <Button onClick={() => setcreateSectionDialog(true)}>
+          + New
+        </Button>
+      </Stack>
+      <Stack direction="column" spacing={2} mb={5}>
+        {sections && sections.map((s) => <SectionCard section={s} enrollment={getEnrollment(s.ID)} />)}
+      </Stack>
+      {sections && sections.length > 0 && <AvailabilitySurvey course={course} />}
+    </>
   );
 }
+
+export default SectionsView;
