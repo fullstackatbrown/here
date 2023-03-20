812d63147391b7090b075d11304904db8bb51bca
Jenny <jennyyu212@gmail.com>
2023-03-20 01:38:32 -0400

    style: grade table

--- frontend/src/components/course/CourseStudentView/StudentGradesTable.tsx
+++ frontend/src/components/course/CourseStudentView/StudentGradesTable.tsx
@@ -1,17 +1,30 @@
 import { GradeChip } from "@components/shared/GradeChip/GradeChip";
 import { Assignment } from "model/assignment";
-import { Table, TableHead, TableRow, TableCell, TableBody, Stack, Box, Chip } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import MuiTableCell from "@mui/material/TableCell";
+import { Table, TableHead, TableRow, TableBody, Stack, Box, Chip } from "@mui/material";
 import { FC } from "react";
 
 interface StudentGradesTableProps {
     assignments: Assignment[];
 }
 
+const TableCell = styled(MuiTableCell)(({ theme }) => ({
+    ":first-of-type": {
+        paddingLeft: 0,
+    },
+    ":last-of-type": {
+        width: 100,
+        maxWidth: 100,
+        overflow: "hidden",
+    },
+}))
+
 const StudentGradesTable: FC<StudentGradesTableProps> = ({ assignments }) => {
     return <Table>
         <TableHead>
             <TableRow>
-                <TableCell sx={{ paddingLeft: 0 }}>
+                <TableCell>
                     {/* <Typography variant="body2">Assignment</Typography> */}
                     Assignment
                 </TableCell>
@@ -23,7 +36,7 @@ const StudentGradesTable: FC<StudentGradesTableProps> = ({ assignments }) => {
         <TableBody>
             {assignments.map((assignment) => (
                 <TableRow key={assignment.ID}>
-                    <TableCell component="th" scope="row" sx={{ paddingLeft: 0 }}>
+                    <TableCell component="th" scope="row">
                         <Stack direction="row" spacing={1} alignItems="center">
                             <Box>{assignment.name}</Box>
                             {assignment.optional && <Chip label="optional" variant="outlined" size="small" color="primary" />}
@@ -35,7 +48,7 @@ const StudentGradesTable: FC<StudentGradesTableProps> = ({ assignments }) => {
                     <TableCell component="th" scope="row">
                         Section name
                     </TableCell>
-                    <TableCell component="th" scope="row">
+                    <TableCell component="th" scope="row" >
                         <GradeChip score={1} maxScore={assignment.maxScore} />
                     </TableCell>
                 </TableRow>
