6ce7150fc766af48c97e9fa99d57a345c0a02c04
Nathan Andrews <nandrews.programming@gmail.com>
2023-04-06 10:12:55 -0400

    fixed mock data fields

--- frontend/src/util/swaps/hooks.ts
+++ frontend/src/util/swaps/hooks.ts
@@ -5,7 +5,6 @@ export const dummySwapRequest1: SwapRequest = {
   studentID: "unnamed student #667",
   oldSectionID: "section1",
   newSectionID: "section2",
-  isTemporary: true,
   requestTime: new Date(2022, 0, 12),
   reason: "I screen shotted classmate's NFT and now he's out to get me",
   status: "approved",
@@ -17,7 +16,6 @@ export const dummySwapRequest2: SwapRequest = {
   studentID: "unnamed student #97",
   oldSectionID: "section2",
   newSectionID: "section3",
-  isTemporary: true,
   requestTime: new Date(2022, 5, 27),
   reason: "I want to work in a room with windows and natural light",
   status: "denied",
@@ -29,7 +27,6 @@ export const dummySwapRequest3: SwapRequest = {
   studentID: "unnamed student #438",
   oldSectionID: "section3",
   newSectionID: "section1",
-  isTemporary: true,
   requestTime: new Date(2022, 3, 3),
   reason:
     "My phone went off last section when everything was quiet and not I'm too embarrassed to go back :(",
