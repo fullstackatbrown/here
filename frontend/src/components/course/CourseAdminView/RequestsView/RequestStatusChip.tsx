1ee937515e6b196498fe18bde5585c0a3b01deae
Nathan Andrews <nandrews.programming@gmail.com>
2023-04-06 11:37:25 -0400

    added option style prop to RequestStatusChip

--- frontend/src/components/course/CourseAdminView/RequestsView/RequestStatusChip.tsx
+++ frontend/src/components/course/CourseAdminView/RequestsView/RequestStatusChip.tsx
@@ -5,18 +5,19 @@ import { SwapRequestStatus } from "model/swapRequest";
 export interface RequestStatusChipProps {
     status: SwapRequestStatus;
     size?: "small" | "medium";
+    style?: React.CSSProperties
 }
 
-const RequestStatusChip: FC<RequestStatusChipProps> = ({ status, size }) => {
+const RequestStatusChip: FC<RequestStatusChipProps> = ({ status, size, style }) => {
     switch (status) {
         case "approved":
-            return <Chip label="approved" variant="outlined" size={size} color="success" />;
+            return <Chip label="approved" variant="outlined" size={size} color="success" style={style} />;
         case "denied":
-            return <Chip label="denied" variant="outlined" size={size} color="error" />;
+            return <Chip label="denied" variant="outlined" size={size} color="error" style={style} />;
         case "archived":
-            return <Chip label="archived" variant="outlined" size={size} />;
+            return <Chip label="archived" variant="outlined" size={size} style={style} />;
         case "cancelled":
-            return <Chip label="cancelled" variant="outlined" size={size} />;
+            return <Chip label="cancelled" variant="outlined" size={size} style={style} />;
     }
 };
 
