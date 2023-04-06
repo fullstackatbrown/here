db2abcb057fb06a6758cb3fa0afb537a48ee5c5e
Nathan Andrews <nandrews.programming@gmail.com>
2023-04-06 11:24:25 -0400

    added number of pending requests to navigation panel

--- frontend/src/components/course/CourseAdminView/CourseAdminViewNavigation.tsx
+++ frontend/src/components/course/CourseAdminView/CourseAdminViewNavigation.tsx
@@ -1,17 +1,44 @@
-import { Button, Stack } from "@mui/material";
+import { Badge, BadgeProps, Button, Stack, styled } from "@mui/material";
+import { useSwapRequests } from "@util/swaps/hooks";
 import { Views } from "model/general";
 
 export interface CourseAdminViewNavigationProps {
-    setView: (view: Views) => void;
+  setView: (view: Views) => void;
 }
 
-export default function CourseAdminViewNavigation(props: CourseAdminViewNavigationProps) {
-    return (
-        <Stack alignItems="start">
-            <Button variant="text" onClick={() => props.setView("sections")}>Sections</Button>
-            <Button variant="text" onClick={() => props.setView("assignments")}>Assignments</Button>
-            <Button variant="text" onClick={() => props.setView("people")}>People</Button>
-            <Button variant="text" onClick={() => props.setView("requests")}>Requests</Button>
-        </Stack>
-    )
+export default function CourseAdminViewNavigation(
+  props: CourseAdminViewNavigationProps
+) {
+  const [swapRequests, _] = useSwapRequests();
+  const numPendingRequests = swapRequests.filter(
+    (r) => r.status === "pending"
+  ).length;
+
+  return (
+    <Stack alignItems="start">
+      <Button variant="text" onClick={() => props.setView("sections")}>
+        Sections
+      </Button>
+      <Button variant="text" onClick={() => props.setView("assignments")}>
+        Assignments
+      </Button>
+      <Button variant="text" onClick={() => props.setView("people")}>
+        People
+      </Button>
+      <Badge
+        color="secondary"
+        badgeContent={numPendingRequests}
+        sx={{
+          "& .MuiBadge-badge": {
+            right: -8,
+            top: "50%",
+          },
+        }}
+      >
+        <Button variant="text" onClick={() => props.setView("requests")}>
+          Requests
+        </Button>
+      </Badge>
+    </Stack>
+  );
 }
