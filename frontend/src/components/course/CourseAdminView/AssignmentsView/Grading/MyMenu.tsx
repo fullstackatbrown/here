e9dcf47727634a0632c1a38b9ebedca72a9cd59c
Jenny <jennyyu212@gmail.com>
2023-04-07 13:39:29 -0400

    style: select section menu

--- frontend/src/components/course/CourseAdminView/AssignmentsView/Grading/MyMenu.tsx
+++ frontend/src/components/course/CourseAdminView/AssignmentsView/Grading/MyMenu.tsx
@@ -26,23 +26,9 @@ const StyledMenu = styled((props: MenuProps) => (
 ))(({ theme }) => ({
     '& .MuiPaper-root': {
         borderRadius: 6,
-        marginTop: theme.spacing(1),
         minWidth: 180,
-        color:
-            theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
         boxShadow:
             'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
-        '& .MuiMenu-list': {
-            padding: '4px 0',
-        },
-        '& .MuiMenuItem-root': {
-            '&:active': {
-                backgroundColor: alpha(
-                    theme.palette.primary.main,
-                    theme.palette.action.selectedOpacity,
-                ),
-            },
-        },
     },
 }));
 
@@ -66,7 +52,6 @@ const MyMenu: FC<MyMenuProps> = ({ value, options, onSelect, formatOption }) => 
             {formatOption(value)}
         </Button>
         <StyledMenu
-            elevation={0}
             anchorEl={anchorEl}
             open={open}
             onClose={handleClose}
@@ -74,10 +59,9 @@ const MyMenu: FC<MyMenuProps> = ({ value, options, onSelect, formatOption }) => 
         >
             {options.map((val) => {
                 return <>
-                    <MenuItem value={val} key={val} onClick={() => onSelect(val)}>
+                    <MenuItem sx={{ fontSize: 14 }} value={val} key={val} onClick={() => onSelect(val)} >
                         {formatOption(val)}
                     </MenuItem>
-                    <Divider />
                 </>
             })}
         </StyledMenu >
