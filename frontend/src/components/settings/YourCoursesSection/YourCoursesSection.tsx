c0f844de0019707ee7a72d0ea637b3726082f75e
Jenny <jennyyu212@gmail.com>
2023-04-12 12:18:17 -0400

    feat: sort by terms for HTA courses

--- frontend/src/components/settings/YourCoursesSection/YourCoursesSection.tsx
+++ frontend/src/components/settings/YourCoursesSection/YourCoursesSection.tsx
@@ -5,6 +5,7 @@ import { useSession } from "api/auth/hooks";
 import CourseListItem from "../CourseListItem";
 import SettingsSection from "@components/settings/SettingsSection";
 import { CoursePermission } from "model/user";
+import { sortCoursesByTerm } from "@util/shared/sortTerms";
 
 export interface YourCoursesSectionProps {
 }
@@ -17,13 +18,15 @@ const YourCoursesSection: FC<YourCoursesSectionProps> = ({ }) => {
     const [courses, loadingCourses] = useCourses();
     const filteredCourses = courses && courses.filter(course => currentUser?.permissions && (currentUser.permissions[course.ID] === CoursePermission.CourseAdmin));
 
+
     // TODO: sort by terms
     return <SettingsSection taOnly title="Manage your courses" loading={loading || loadingCourses}>
-        {filteredCourses && <List>
-            {filteredCourses.map((course, index) =>
-                <CourseListItem key={course.ID} course={course}
-                    isLastChild={index === (filteredCourses.length - 1)} />)}
-        </List>}
+        {filteredCourses &&
+            <List>
+                {sortCoursesByTerm(filteredCourses).map((course, index) =>
+                    <CourseListItem key={course.ID} course={course}
+                        isLastChild={index === (filteredCourses.length - 1)} />)}
+            </List>}
     </SettingsSection>;
 };
 
