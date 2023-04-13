62ed8811002fc54f009da64ac67c21a93cc0ebca
Jenny <jennyyu212@gmail.com>
2023-04-13 13:47:55 -0400

    feat: protect /admin route

--- frontend/src/pages/manage.tsx
+++ frontend/src/pages/manage.tsx
@@ -1,14 +1,22 @@
 import AdminList from "@components/manage/AdminList/AdminList";
 import AllCourses from "@components/manage/AllCourses/AllCourses";
 import AppLayout from "@components/shared/AppLayout";
-import { Stack } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
+import { useSession } from "api/auth/hooks";
 
 export default function Manage() {
+    const { currentUser, loading } = useSession();
+
     return (
-        <AppLayout maxWidth="md">
+        <AppLayout maxWidth="md" loading={loading}>
             <Stack spacing={5} mt={15}>
-                <AllCourses />
-                <AdminList />
+                {currentUser && currentUser?.isAdmin ?
+                    <>
+                        <AllCourses />
+                        <AdminList />
+                    </> :
+                    <Typography textAlign="center" variant="h6">Sorry, you don't have the permissions to access this page.</Typography>
+                }
             </Stack>
         </AppLayout>
     );
