63cc28a698d11f6101f1670b83b624cf8b8198d8
Hammad Izhar <hammad_izhar@brown.edu>
2023-04-15 10:34:51 -0400

    add tooltip to menu icon

--- frontend/src/components/course/CourseAdminView/CourseAdminViewNavigation.tsx
+++ frontend/src/components/course/CourseAdminView/CourseAdminViewNavigation.tsx
@@ -1,26 +1,11 @@
-import {
-  Badge,
-  Box,
-  Button,
-  Divider,
-  Drawer,
-  Grid,
-  IconButton,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Menu,
-  Stack,
-  Link,
-  Typography,
-} from "@mui/material";
+import { Badge, Box, Button, Divider, Drawer, Stack, Link } from "@mui/material";
+import IconButton from "@components/shared/IconButton";
 import { capitalizeFirstLetter } from "@util/shared/string";
 import { usePendingSwaps } from "api/swaps/hooks";
 import { View } from "model/general";
 import { CoursePermission } from "model/user";
 import { useRouter } from "next/router";
 import MenuIcon from "@mui/icons-material/Menu";
-import CloseIcon from "@mui/icons-material/Close";
 import { useState } from "react";
 import Logo from "@components/shared/Logo";
 import NextLink from "next/link";
@@ -39,7 +24,7 @@ export default function CourseAdminViewNavigation({ access }: CourseAdminViewNav
     return router.push(`${router.query.courseID}?view=${view}`, undefined, { shallow: true });
   }
 
-  function getNavigationButton(view: View, toggleDrawer?: () => void) {
+  function getNavigationButton(view: View) {
     return (
       <Button
         sx={{
@@ -50,7 +35,7 @@ export default function CourseAdminViewNavigation({ access }: CourseAdminViewNav
         color={query.view === view ? "inherit" : "secondary"}
         variant="text"
         onClick={() => {
-          if (toggleDrawer) toggleDrawer();
+          toggleOpen(false);
           navigateTo(view);
         }}
       >
@@ -59,87 +44,9 @@ export default function CourseAdminViewNavigation({ access }: CourseAdminViewNav
     );
   }
 
-  return (
-    <>
-      <IconButton
-        sx={{
-          mr: 2,
-          display: {
-            position: "fixed",
-            xs: "block",
-            md: "none",
-          },
-        }}
-        onClick={() => toggleOpen(true)}
-      >
-        <MenuIcon />
-      </IconButton>
-      <Drawer open={open} onClose={() => toggleOpen(false)}>
-        <Box
-          sx={{
-            width: "max-content",
-          }}
-        >
-          {/* <Stack flexDirection="row" alignItems="center" px={4} py={2}>
-            <Box mr={1} width={30} height={30}>
-              <Logo />
-            </Box>
-            <Typography variant="h6">Here</Typography>
-          </Stack> */}
-
-          <Stack flexDirection="row" alignItems="center" px={4} py={2}>
-            <NextLink href="/">
-              <Link
-                variant="h6"
-                component="button"
-                color="inherit"
-                underline="hover"
-                sx={{ display: "inline-flex", alignItems: "center" }}
-              >
-                <Box mr={1} width={30} height={30}>
-                  <Logo />
-                </Box>
-                Here
-              </Link>
-            </NextLink>
-          </Stack>
-
-          <Divider />
-
-          <Stack alignItems="start" px={4}>
-            {getNavigationButton("sections", () => toggleOpen(false))}
-            {getNavigationButton("assignments", () => toggleOpen(false))}
-            {getNavigationButton("people", () => toggleOpen(false))}
-            {pendingRequests && pendingRequests.length > 0 ? (
-              <Badge
-                color="primary"
-                badgeContent={pendingRequests.length}
-                sx={{
-                  "& .MuiBadge-badge": {
-                    right: -10,
-                    top: "50%",
-                  },
-                }}
-              >
-                {getNavigationButton("requests", () => toggleOpen(false))}
-              </Badge>
-            ) : (
-              getNavigationButton("requests", () => toggleOpen(false))
-            )}
-            {access === CoursePermission.CourseAdmin && getNavigationButton("settings", () => toggleOpen(false))}
-          </Stack>
-        </Box>
-      </Drawer>
-      <Stack
-        alignItems="start"
-        sx={{
-          position: "fixed",
-          display: {
-            xs: "none",
-            md: "flex",
-          },
-        }}
-      >
+  function getSidebarItems() {
+    return (
+      <>
         {getNavigationButton("sections")}
         {getNavigationButton("assignments")}
         {getNavigationButton("people")}
@@ -160,6 +67,67 @@ export default function CourseAdminViewNavigation({ access }: CourseAdminViewNav
           getNavigationButton("requests")
         )}
         {access === CoursePermission.CourseAdmin && getNavigationButton("settings")}
+      </>
+    );
+  }
+
+  return (
+    <>
+      <IconButton
+        label="Menu"
+        sx={{
+          mr: 2,
+          display: {
+            position: "fixed",
+            xs: "block",
+            md: "none",
+          },
+        }}
+        onClick={() => toggleOpen(true)}
+      >
+        <MenuIcon />
+      </IconButton>
+      <Drawer open={open} onClose={() => toggleOpen(false)}>
+        <Box
+          sx={{
+            width: "max-content",
+          }}
+        >
+          <Stack flexDirection="row" alignItems="center" px={4} py={2}>
+            <NextLink href="/">
+              <Link
+                variant="h6"
+                component="button"
+                color="inherit"
+                underline="hover"
+                sx={{ display: "inline-flex", alignItems: "center" }}
+              >
+                <Box mr={1} width={30} height={30}>
+                  <Logo />
+                </Box>
+                Here
+              </Link>
+            </NextLink>
+          </Stack>
+
+          <Divider />
+
+          <Stack alignItems="start" px={4}>
+            {getSidebarItems()}
+          </Stack>
+        </Box>
+      </Drawer>
+      <Stack
+        alignItems="start"
+        sx={{
+          position: "fixed",
+          display: {
+            xs: "none",
+            md: "flex",
+          },
+        }}
+      >
+        {getSidebarItems()}
       </Stack>
     </>
   );
