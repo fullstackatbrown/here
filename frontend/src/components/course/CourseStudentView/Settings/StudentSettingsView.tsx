9f5d7d711326a22160405462aaa3c9f2a2929221
Jenny <jennyyu212@gmail.com>
2023-04-21 13:35:05 -0400

    feat: quit course

--- frontend/src/components/course/CourseStudentView/Settings/StudentSettingsView.tsx
+++ frontend/src/components/course/CourseStudentView/Settings/StudentSettingsView.tsx
@@ -1,28 +1,43 @@
-import { Stack } from "@mui/material";
-import { Assignment } from "model/assignment";
+import { Button } from "@mui/material";
 import { Course } from "model/course";
-import { Section } from "model/section";
-import { User } from "model/user";
 import { FC } from "react";
 import StudentViewHeader from "../StudentViewHeader";
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import AuthAPI from "api/auth/api";
+import { useRouter } from "next/router";
+import toast from "react-hot-toast";
+import { handleBadRequestError } from "@util/errors";
 
 export interface StudentSettingsViewProps {
     course: Course;
 }
 
 const StudentSettingsView: FC<StudentSettingsViewProps> = ({ course }) => {
+    const router = useRouter();
 
     const quitCourse = () => {
-        const confirmed = confirm("Are you sure you want to quit this course?");
+        const confirmed = confirm("Are you sure you want to quit this course? You will no longer be able to access this course.");
         if (confirmed) {
+            AuthAPI.quitCourse(course.ID)
+                .then(() => {
+                    router.push("/").then(() => toast.success(`Successfully quit from ${course.code}`));
+                })
+                .catch((err) => {
+                    toast.error(handleBadRequestError(err))
+                });
         }
     };
 
     return (
         <>
             <StudentViewHeader view="settings" />
-            <Stack direction="row" justifyContent="space-between" spacing={1}>
-            </Stack>
+            <Button
+                variant="outlined"
+                startIcon={<ExitToAppIcon />}
+                onClick={() => quitCourse()}
+            >
+                Quit Course
+            </Button>
 
         </>
     );
