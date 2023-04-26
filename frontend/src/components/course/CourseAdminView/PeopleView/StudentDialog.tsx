7cb521c43cabfb2189c2a9ae87aa8dc1675dda3d
Jenny <jennyyu212@gmail.com>
2023-04-25 23:54:58 -0400

    feat: UI for changing default section

--- frontend/src/components/course/CourseAdminView/PeopleView/StudentDialog.tsx
+++ frontend/src/components/course/CourseAdminView/PeopleView/StudentDialog.tsx
@@ -1,12 +1,19 @@
 import StudentGradesTable from "@components/course/CourseStudentView/Home/StudentGradesTable";
+import SelectMenu from "@components/shared/Menu/SelectMenu";
+import CheckIcon from '@mui/icons-material/Check';
+import CloseIcon from '@mui/icons-material/Close';
+import EditIcon from '@mui/icons-material/Edit';
 import {
-    Box,
     Dialog,
     DialogContent,
     DialogTitle,
+    IconButton,
+    Stack,
+    Tooltip,
     Typography
 } from "@mui/material";
 import formatSectionInfo from "@util/shared/formatSectionInfo";
+import { UNASSIGNED } from "@util/shared/getStudentsInSection";
 import AuthAPI from "api/auth/api";
 import { Assignment } from "model/assignment";
 import { Course } from "model/course";
@@ -24,8 +31,9 @@ export interface StudentDialogProps {
 }
 
 const StudentDialog: FC<StudentDialogProps> = ({ course, studentID, assignments, sectionsMap, open, onClose }) => {
-
     const [student, setStudent] = useState<User | undefined>(undefined);
+    const defaultSectionID = () => student?.defaultSections?.[course.ID]
+    const [selectedSection, setSelectedSection] = useState<string | undefined>(undefined);
 
     useEffect(() => {
         if (studentID && studentID != "") {
@@ -38,23 +46,87 @@ const StudentDialog: FC<StudentDialogProps> = ({ course, studentID, assignments,
 
     const handleOnClose = () => {
         onClose();
+        setSelectedSection(undefined);
+        setStudent(undefined);
     };
 
-    const getDefaultSection = () => {
-        const defaultSection = student?.defaultSections?.[course.ID]
-        if (defaultSection && defaultSection !== "" && sectionsMap) {
-            return formatSectionInfo(sectionsMap[defaultSection], true)
+
+    const sectionOptions = () => {
+        let options = [UNASSIGNED]
+        sectionsMap && Object.keys(sectionsMap).forEach((sectionID) => {
+            options.push(sectionID)
+        })
+        return options
+    }
+
+    const formatOptions = (val: string | undefined) => {
+        if (val === UNASSIGNED || val === undefined) return UNASSIGNED
+        const section = sectionsMap[val] as Section
+        const seatsAvail = section.capacity - section.numEnrolled
+        const current = val === defaultSectionID()
+        return `${formatSectionInfo(section, true)} (${current ? "current" : seatsAvail + " seats available"})`
+    }
+
+    const reset = () => {
+        setSelectedSection(undefined)
+    }
+
+    const openEditMode = () => {
+        setSelectedSection(defaultSectionID())
+    }
+
+    const handleChangeDefaultSection = () => {
+        if (selectedSection === defaultSectionID()) {
+            setSelectedSection(undefined)
+            return
         }
-        return undefined
+
+
+
     }
 
     return student && <Dialog open={open} onClose={handleOnClose} fullWidth maxWidth="md" keepMounted={false}>
         <DialogTitle>{student.displayName} ({student.email})</DialogTitle>
         <DialogContent>
-            <Typography fontSize={15}>
-                <Box component="span" fontWeight={500}>Regular Section:</Box>&nbsp;
-                {getDefaultSection() || "Unassigned"}
-            </Typography>
+            <Stack direction="row" spacing={1} alignItems="center" justifyContent="space-between">
+                <Typography variant="button" fontSize={15} fontWeight={500}>Regular Section:&nbsp;</Typography>
+                <Stack direction="row" alignItems="center">
+                    {selectedSection !== undefined ?
+                        <SelectMenu
+                            value={selectedSection}
+                            formatOption={formatOptions}
+                            options={sectionOptions()}
+                            onSelect={(val) => setSelectedSection(val)}
+                            defaultValue={defaultSectionID()}
+                        />
+                        :
+                        <Typography color="secondary" variant="button" fontSize={14} mr={1}>
+                            {formatSectionInfo(sectionsMap[defaultSectionID()], true) || "Unassigned"}
+                        </Typography>
+                    }
+                    {selectedSection !== undefined ?
+                        <Stack direction="row" alignItems="center" spacing={0.5}>
+                            <Tooltip title="confirm" placement="bottom">
+                                <IconButton sx={{ p: 0.5 }} onClick={handleChangeDefaultSection}>
+                                    <CheckIcon sx={{ fontSize: 18 }} />
+                                </IconButton>
+                            </Tooltip>
+                            <Tooltip title="discard" placement="bottom">
+                                <IconButton sx={{ p: 0.5 }} onClick={reset}>
+                                    <CloseIcon sx={{ fontSize: 18 }} />
+                                </IconButton>
+                            </Tooltip>
+                        </Stack>
+                        :
+                        <Tooltip title={`change section for ${student.displayName}`} placement="bottom">
+                            <IconButton sx={{ p: 0.5 }} onClick={openEditMode}>
+                                <EditIcon sx={{ fontSize: 18 }} />
+                            </IconButton>
+                        </Tooltip>
+
+                    }
+                </Stack>
+            </Stack>
             <StudentGradesTable {...{ course, student, sectionsMap, assignments }} />
         </DialogContent>
     </Dialog >;
