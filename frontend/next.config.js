711f3863f53d2b6c288991ba6a00f1c207ab0048
Tianren Dong <tianrendong.dtr@gmail.com>
2023-09-06 11:59:02 -0400

    feat: next config reverse proxy

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: false,
+  async rewrites() {
+    return [
+      {
+        source: "/__/auth",
+        destination: "https://here-c8872.firebaseapp.com/__/auth",
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
