f7e494fdfefb2892fe50e86f151b74f6405453db
Tianren Dong <tianrendong.dtr@gmail.com>
2023-09-09 18:39:29 -0400

    style: combine two maps resCount and resStudents

--- frontend/src/util/shared/survey.ts
+++ frontend/src/util/shared/survey.ts
@@ -17,26 +17,16 @@ export type OptionDetails = {
 }
 
 export default function formatSurveyResponses(options: SurveyOption[], responses: Record<string, string[]>): OptionDetails[] {
-    // Map from option to number of students who chose that option
-    let resCount: Record<string, number> = {};
-
     // Map from option to list of students who chose that option
     let resStudents: Record<string, string[]> = {};
 
     for (const option of options) {
-        resCount[option.option] = 0;
         resStudents[option.option] = [];
     }
 
     for (const studentID in responses) {
         const times = responses[studentID];
         for (const time of times) {
-            // Increment count for this option
-            if (resCount[time] === undefined) {
-                resCount[time] = 1
-            } else {
-                resCount[time] += 1
-            }
             // Add student to list of students who chose this option
             if (resStudents[time] === undefined) {
                 resStudents[time] = [studentID];
@@ -47,9 +37,9 @@ export default function formatSurveyResponses(options: SurveyOption[], responses
     }
 
     let list: OptionDetails[] = [];
-    for (const option in resCount) {
+    for (const option in resStudents) {
         list.push({
-            option: option, count: resCount[option],
+            option: option, count: resStudents[option].length,
             students: resStudents[option],
             optionExists: options.find(o => o.option === option) !== undefined
         })
